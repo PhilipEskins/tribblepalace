9af3295d58687abd5b7bf4c7d7291bb145f90df1
Philip Eskins <philipeskins@gmail.com>
2019-03-01 10:37:56 -0800

    finish making webpack environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,9 +9,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, "dist")
   },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
 
   module: {
-    rules: [    
+    rules: [
       {
         test: /\.scss$/,
         use: [
@@ -17,7 +23,26 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
-      }
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPatch: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        loader: 'html-srcsets-loader',
+        options: {
+          attrs: ['img:src', ':srcset'],
+        }
+      },
     ]
   },
 
@@ -25,7 +50,13 @@ module.exports = {
     new HtmlWebpackPlugin ({
       inject: 'body',
       template: './src/index.html',
-      filename: 'index.html'
-    })
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(['dist'])
   ]
 };
